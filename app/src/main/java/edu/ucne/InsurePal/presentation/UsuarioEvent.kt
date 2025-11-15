4e2e6ac87f44966a6c36e0c1522fbcc2dbbb566a
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-15 13:56:31 -0400

    Crea la clase sellada UsuarioEvent

---
+++ app/src/main/java/edu/ucne/InsurePal/presentation/UsuarioEvent.kt
@@ -0,0 +1,18 @@
+package edu.ucne.InsurePal.presentation
+
+import edu.ucne.InsurePal.domain.Usuario
+
+sealed interface UsuarioEvent
+{
+    data object cargar : UsuarioEvent
+    data class onUsernameChange(val userName: String) : UsuarioEvent
+    data class onPasswordChange(val password: String) : UsuarioEvent
+    data class obtener(val id: Int?): UsuarioEvent
+    object userMessageShown : UsuarioEvent
+
+
+    data class crear(val usuario: Usuario): UsuarioEvent
+    data class actualizar(val usuario: Usuario) : UsuarioEvent
+
+    data object new: UsuarioEvent
+}
