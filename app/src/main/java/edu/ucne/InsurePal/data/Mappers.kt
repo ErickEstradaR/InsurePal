acc5a2b5379e30bbc85be3ed4fe9e694bdf86f14
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-15 13:14:41 -0400

    Crea los mappers para Usuario

---
+++ app/src/main/java/edu/ucne/InsurePal/data/Mappers.kt
@@ -0,0 +1,16 @@
+package edu.ucne.InsurePal.data
+
+import edu.ucne.InsurePal.data.remote.usuario.dto.UsuarioRequest
+import edu.ucne.InsurePal.data.remote.usuario.dto.UsuarioResponse
+import edu.ucne.InsurePal.domain.Usuario
+
+fun Usuario.toRequest(): UsuarioRequest = UsuarioRequest(
+    userName = userName,
+    password = password
+)
+
+fun UsuarioResponse.toDomain() = Usuario(
+    usuarioId = usuarioId,
+    userName = userName,
+    password = password
+)
