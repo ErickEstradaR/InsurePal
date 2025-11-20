e16f88a05361b7c2bfc7259693280a0b552d7369
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-20 18:20:51 -0400

    Crea la interfaz del repositorio `SeguroVehiculoRepository`

---
+++ app/src/main/java/edu/ucne/InsurePal/domain/polizas/vehiculo/repository/SeguroVehiculoRepository.kt
@@ -0,0 +1,13 @@
+package edu.ucne.InsurePal.domain.polizas.vehiculo.repository
+
+import edu.ucne.InsurePal.data.Resource
+import edu.ucne.InsurePal.data.remote.polizas.vehiculo.dto.SeguroVehiculoRequest
+import edu.ucne.InsurePal.domain.polizas.vehiculo.model.SeguroVehiculo
+import kotlinx.coroutines.flow.Flow
+
+interface SeguroVehiculoRepository {
+    suspend fun getVehiculos(): Flow<Resource<List<SeguroVehiculo>>>
+    suspend fun getVehiculo(id: Int?): Flow<Resource<SeguroVehiculo>>
+    suspend fun postVehiculo(req: SeguroVehiculoRequest): Resource<SeguroVehiculo>
+    suspend fun putVehiculo(id: Int, req: SeguroVehiculoRequest): Resource<Unit>
+}
