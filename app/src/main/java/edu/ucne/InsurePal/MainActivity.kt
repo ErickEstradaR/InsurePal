2b06460b45464360e52217fb72b6c3e780f5701e
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-20 12:43:59 -0400

    Reemplaza `UsuarioScreen` con `LoginScreen`

--- app/src/main/java/edu/ucne/InsurePal/MainActivity.kt
+++ app/src/main/java/edu/ucne/InsurePal/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.InsurePal.presentation.usuario.UsuarioScreen
+import edu.ucne.InsurePal.presentation.usuario.LoginScreen
 import edu.ucne.InsurePal.ui.theme.InsurePalTheme
 
 @AndroidEntryPoint
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    UsuarioScreen()
+                    LoginScreen()
                 }
             }
         }
