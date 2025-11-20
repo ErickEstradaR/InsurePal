0606817512ca6f267a78dc08e4dcb95b974284a8
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-20 18:20:31 -0400

    Crea los mappers para SeguroVehiculo

--- app/src/main/java/edu/ucne/InsurePal/data/Mappers.kt
+++ app/src/main/java/edu/ucne/InsurePal/data/Mappers.kt
@@ -1,8 +1,12 @@
 package edu.ucne.InsurePal.data
 
+import edu.ucne.InsurePal.data.remote.polizas.vehiculo.dto.SeguroVehiculoRequest
+import edu.ucne.InsurePal.data.remote.polizas.vehiculo.dto.SeguroVehiculoResponse
 import edu.ucne.InsurePal.data.remote.usuario.dto.UsuarioRequest
 import edu.ucne.InsurePal.data.remote.usuario.dto.UsuarioResponse
-import edu.ucne.InsurePal.domain.usuario.Usuario
+import edu.ucne.InsurePal.domain.polizas.vehiculo.model.SeguroVehiculo
+import edu.ucne.InsurePal.domain.usuario.model.Usuario
+
 
 fun Usuario.toRequest(): UsuarioRequest = UsuarioRequest(
     userName = userName,
@@ -13,4 +17,23 @@ fun UsuarioResponse.toDomain() = Usuario(
     usuarioId = usuarioId,
     userName = userName,
     password = password
+)
+
+fun SeguroVehiculoResponse.toDomain() = SeguroVehiculo(
+     idPoliza  = idPoliza,
+     name = name,
+     status = status,
+     expirationDate = expirationDate,
+     placa = placa,
+     modeloVehiculo= modeloVehiculo,
+     coverageType = coverageType
+)
+
+fun SeguroVehiculo.toRequest(): SeguroVehiculoRequest = SeguroVehiculoRequest(
+    name = name,
+    status = status,
+    expirationDate = expirationDate,
+    placa = placa,
+    modeloVehiculo= modeloVehiculo,
+    coverageType = coverageType
 )
