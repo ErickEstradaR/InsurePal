1d5cca4fcdd6c38f384293aec41860986364f5c7
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-21 12:22:28 -0400

    Navegación para la pantalla de inicio

---
+++ app/src/main/java/edu/ucne/InsurePal/presentation/navigation/InsurePalNavigation.kt
@@ -0,0 +1,38 @@
+package edu.ucne.InsurePal.presentation.navigation
+
+import android.widget.Toast
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import edu.ucne.InsurePal.presentation.home.InsuranceHomeScreen
+import edu.ucne.InsurePal.presentation.usuario.LoginScreen
+
+@Composable
+fun InsurePalNavigation() {
+    val navController = rememberNavController()
+    val context = LocalContext.current
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Login.route
+    ) {
+
+        composable(Screen.Login.route) {
+            LoginScreen(
+                onLoginSuccess = {
+
+                    navController.navigate(Screen.Home.route) {
+                          popUpTo(Screen.Login.route) { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        composable(Screen.Home.route) {
+            InsuranceHomeScreen(
+            )
+        }
+    }
+}
