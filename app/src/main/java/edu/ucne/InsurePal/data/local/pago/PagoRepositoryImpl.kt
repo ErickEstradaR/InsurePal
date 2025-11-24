df9645031cddf0778b4638a159a009462561ed77
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-24 16:04:47 -0400

    Obtiene el ID de usuario desde UserPreferences

--- app/src/main/java/edu/ucne/InsurePal/data/local/pago/PagoRepositoryImpl.kt
+++ app/src/main/java/edu/ucne/InsurePal/data/local/pago/PagoRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package edu.ucne.InsurePal.data.local.pago
 
 import edu.ucne.InsurePal.data.Resource
+import edu.ucne.InsurePal.data.local.UserPreferences
 import edu.ucne.InsurePal.data.remote.pago.PagoRemoteDataSource
 import edu.ucne.InsurePal.data.remote.pago.dto.PagoRequest
 import edu.ucne.InsurePal.data.toDomain
@@ -8,14 +9,15 @@ import edu.ucne.InsurePal.domain.pago.model.Pago
 import edu.ucne.InsurePal.domain.pago.model.TarjetaCredito
 import edu.ucne.InsurePal.domain.pago.repository.PagoRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import java.time.LocalDateTime
 import javax.inject.Inject
 
 class PagoRepositoryImpl @Inject constructor(
     private val remoteDataSource: PagoRemoteDataSource,
-    private val pagoDao: PagoDao
-    // private val userPreferences: UserPreferences // TODO: Inyectar esto para obtener el ID real
+    private val pagoDao: PagoDao,
+    private val userPreferences: UserPreferences
 ) : PagoRepository {
 
     override fun getHistorialPagos(usuarioId: Int): Flow<List<Pago>> {
@@ -31,8 +33,7 @@ class PagoRepositoryImpl @Inject constructor(
         tarjeta: TarjetaCredito
     ): Resource<Pago> {
 
-        // TODO: Obtener el ID del usuario logueado desde UserPreferences
-        val usuarioIdActual = 3
+        val usuarioIdActual = userPreferences.userId.first() ?: 0
 
         val request = PagoRequest(
             polizaId = polizaId,
