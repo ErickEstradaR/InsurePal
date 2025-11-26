94dc77e461efcf33df9d5e8f9b8a4cb4de8e6372
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-26 13:27:34 -0400

    Actualiza el estado de la póliza después del pago

--- app/src/main/java/edu/ucne/InsurePal/presentation/pago/PagoViewModel.kt
+++ app/src/main/java/edu/ucne/InsurePal/presentation/pago/PagoViewModel.kt
@@ -7,15 +7,20 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import edu.ucne.InsurePal.data.Resource
 import edu.ucne.InsurePal.domain.pago.model.TarjetaCredito
 import edu.ucne.InsurePal.domain.pago.useCase.ProcesarPagoUseCase
+import edu.ucne.InsurePal.domain.polizas.vehiculo.repository.SeguroVehiculoRepository
+import edu.ucne.InsurePal.domain.polizas.vida.repository.SeguroVidaRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.time.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
 class PagoViewModel @Inject constructor(
     private val procesarPago: ProcesarPagoUseCase,
+    private val vehiculoRepository: SeguroVehiculoRepository,
+    private val vidaRepository: SeguroVidaRepository,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
@@ -23,14 +28,10 @@ class PagoViewModel @Inject constructor(
     val state = _state.asStateFlow()
 
     private val polizaId: String = savedStateHandle.get<String>("polizaId") ?: ""
-
     private val monto: Double = savedStateHandle.get<Double>("monto") ?: 0.0
-
     private val descripcion: String = savedStateHandle.get<String>("descripcion") ?: "Pago de Póliza"
 
     init {
-
-
         _state.update { it.copy(polizaId = polizaId, montoAPagar = monto) }
     }
 
@@ -73,6 +74,9 @@ class PagoViewModel @Inject constructor(
 
             when(result) {
                 is Resource.Success -> {
+                    // Si cobró, actualizamos la póliza
+                    actualizarEstadoPoliza(uiState.polizaId)
+
                     _state.update { it.copy(isLoading = false, isSuccess = true) }
                 }
                 is Resource.Error -> {
@@ -80,6 +84,42 @@ class PagoViewModel @Inject constructor(
                 }
                 is Resource.Loading -> _state.update { it.copy(isLoading = true) }
             }
+        }
+    }
+
+    private suspend fun actualizarEstadoPoliza(id: String) {
+        try {
+            val nuevaFechaPago = LocalDate.now().plusMonths(1).toString()
+
+            if (id.startsWith("VIDA-")) {
+                val resultGet = vidaRepository.getSeguroVidaById(id)
+                if (resultGet is Resource.Success) {
+                    val seguroVida = resultGet.data
+                    if (seguroVida != null) {
+                        val actualizado = seguroVida.copy(
+                            esPagado = true,
+                            fechaPago = nuevaFechaPago
+                        )
+                        vidaRepository.updateSeguroVida(id, actualizado)
+                    }
+                }
+
+            } else {
+                val resultGet = vehiculoRepository.getVehiculo(id)
+                if (resultGet is Resource.Success) {
+                    val seguroVehiculo = resultGet.data
+                    if (seguroVehiculo != null) {
+                        val actualizado = seguroVehiculo.copy(
+                            esPagado = true,
+                            fechaPago = nuevaFechaPago,
+                            status = "Mensualidad Paga"
+                        )
+                        vehiculoRepository.putVehiculo(id, actualizado)
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
