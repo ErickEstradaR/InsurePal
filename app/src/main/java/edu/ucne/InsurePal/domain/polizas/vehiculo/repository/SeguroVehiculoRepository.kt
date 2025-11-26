3321aa24999d4b0b92917209f01e213165657b0b
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-26 11:24:12 -0400

    Modifica la interfaz SeguroVehiculoRepository

--- app/src/main/java/edu/ucne/InsurePal/domain/polizas/vehiculo/repository/SeguroVehiculoRepository.kt
+++ app/src/main/java/edu/ucne/InsurePal/domain/polizas/vehiculo/repository/SeguroVehiculoRepository.kt
@@ -3,13 +3,19 @@ package edu.ucne.InsurePal.domain.polizas.vehiculo.repository
 import edu.ucne.InsurePal.data.Resource
 import edu.ucne.InsurePal.data.remote.polizas.vehiculo.dto.SeguroVehiculoRequest
 import edu.ucne.InsurePal.domain.polizas.vehiculo.model.SeguroVehiculo
+import edu.ucne.InsurePal.domain.polizas.vida.model.SeguroVida
 import kotlinx.coroutines.flow.Flow
 
 interface SeguroVehiculoRepository {
-    suspend fun getVehiculos(usuarioId: Int): Flow<Resource<List<SeguroVehiculo>>>
-    suspend fun getVehiculo(id: String?): Flow<Resource<SeguroVehiculo>>
-    suspend fun postVehiculo(req: SeguroVehiculoRequest): Resource<SeguroVehiculo>
-    suspend fun putVehiculo(id: String, req: SeguroVehiculoRequest): Resource<Unit>
+
+
+    fun getVehiculos(usuarioId: Int): Flow<Resource<List<SeguroVehiculo>>>
+
+    suspend fun getVehiculo(id: String): Resource<SeguroVehiculo>
+
+    suspend fun postVehiculo(seguro: SeguroVehiculo): Resource<SeguroVehiculo>
+
+    suspend fun putVehiculo(id: String, seguro: SeguroVehiculo): Resource<Unit>
 
     suspend fun delete(id: String) : Resource<Unit>
 }
