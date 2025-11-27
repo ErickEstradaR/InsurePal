e4d3bed032b76e7ca72001f3fe6b6ca784188918
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-27 14:44:22 -0400

    Actualiza el `AdminUiState` para las estadísticas

--- app/src/main/java/edu/ucne/InsurePal/presentation/admin/AdminUiState.kt
+++ app/src/main/java/edu/ucne/InsurePal/presentation/admin/AdminUiState.kt
@@ -1,11 +1,14 @@
 package edu.ucne.InsurePal.presentation.admin
 
-import edu.ucne.InsurePal.presentation.home.uiModels.PolicyUiModel
 
 data class AdminUiState (
     val isLoading: Boolean = false,
-    val policies: List<PolicyUiModel> = emptyList(),
     val errorMessage: String? = null,
-    val selectedPolicy: PolicyUiModel? = null,
-    val isDetailVisible: Boolean = false
+    val totalPolicies: Int = 0,
+    val totalVehicles: Int = 0,
+    val totalLife: Int = 0,
+    val activeCount: Int = 0,
+    val pendingCount: Int = 0,
+    val totalCoverageValue: Double = 0.0
 )
+
