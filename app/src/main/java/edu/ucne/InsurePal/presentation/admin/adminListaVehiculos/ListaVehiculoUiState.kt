8f6c8c18c83ccc3e469f9c154a7a335a26f70e96
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-27 15:02:03 -0400

    Crea el UiState ListaVehiculoUiState

---
+++ app/src/main/java/edu/ucne/InsurePal/presentation/admin/adminListaVehiculos/ListaVehiculoUiState.kt
@@ -0,0 +1,13 @@
+package edu.ucne.InsurePal.presentation.admin.adminListaVehiculos
+
+import edu.ucne.InsurePal.domain.polizas.vehiculo.model.SeguroVehiculo
+
+data class ListaVehiculoUiState (
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null,
+    val vehicles: List<SeguroVehiculo> = emptyList(),
+    val filteredVehicles: List<SeguroVehiculo> = emptyList(),
+    val searchQuery: String = "",
+    val selectedVehicle: SeguroVehiculo? = null,
+    val isDetailVisible: Boolean = false
+)
