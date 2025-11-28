49c815e1eff45b12ae6dc34da490d5472c988f6e
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-28 12:13:16 -0400

    Agrega los eventos OnUpdateStatus y OnTogglePendingFilter a ListaVehiculoEvent

--- app/src/main/java/edu/ucne/InsurePal/presentation/admin/adminListaVehiculos/ListaVehiculoEvent.kt
+++ app/src/main/java/edu/ucne/InsurePal/presentation/admin/adminListaVehiculos/ListaVehiculoEvent.kt
@@ -5,6 +5,8 @@ import edu.ucne.InsurePal.domain.polizas.vehiculo.model.SeguroVehiculo
 sealed interface ListaVehiculoEvent {
     data class OnSearchQueryChange(val query: String) : ListaVehiculoEvent
     data class OnSelectVehicle(val vehicle: SeguroVehiculo) : ListaVehiculoEvent
+    data class OnUpdateStatus(val vehicle: SeguroVehiculo, val newStatus: String) : ListaVehiculoEvent
     data object OnDismissDetail : ListaVehiculoEvent
     data object Refresh : ListaVehiculoEvent
+    data object OnTogglePendingFilter : ListaVehiculoEvent
 }
