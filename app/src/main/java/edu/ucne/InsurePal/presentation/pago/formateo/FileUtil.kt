e00c017805ac6ab24fbe38394b67813186f4dbd6
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-28 17:29:48 -0400

    Crea la función `crearArchivoDesdeUri` en FileUtil

---
+++ app/src/main/java/edu/ucne/InsurePal/presentation/pago/formateo/FileUtil.kt
@@ -0,0 +1,20 @@
+package edu.ucne.InsurePal.presentation.pago.formateo
+
+import android.content.Context
+import android.net.Uri
+import java.io.File
+
+fun Context.crearArchivoDesdeUri(uri: Uri): File? {
+    return try {
+        val stream = contentResolver.openInputStream(uri)
+        val file = File.createTempFile("evidencia_", ".jpg", cacheDir)
+        stream?.use { input ->
+            file.outputStream().use { output ->
+                input.copyTo(output)
+            }
+        }
+        file
+    } catch (e: Exception) {
+        null
+    }
+}
