efe4dd83f5c67597020c5eec898b15d4279c17a2
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-28 17:30:29 -0400

    Crea la interfaz `ReclamoVehiculoRepository`

---
+++ app/src/main/java/edu/ucne/InsurePal/domain/reclamoVehiculo/repository/ReclamoVehiculoRepository.kt
@@ -0,0 +1,32 @@
+package edu.ucne.InsurePal.domain.reclamoVehiculo.repository
+
+import edu.ucne.InsurePal.data.Resource
+import edu.ucne.InsurePal.domain.reclamoVehiculo.model.ReclamoVehiculo
+import java.io.File
+
+interface ReclamoVehiculoRepository {
+    suspend fun crearReclamoVehiculo(
+        polizaId: String,
+        usuarioId: Int,
+        descripcion: String,
+        direccion: String,
+        tipoIncidente: String,
+        fechaIncidente: String,
+        imagen: File
+    ): Resource<ReclamoVehiculo>
+
+    suspend fun cambiarEstadoReclamoVehiculo(
+        reclamoId: String,
+        nuevoEstado: String,
+        motivoRechazo: String?
+    ): Resource<ReclamoVehiculo>
+
+
+    suspend fun obtenerReclamoVehiculos(
+        usuarioId: Int? = null
+    ): Resource<List<ReclamoVehiculo>>
+
+    suspend fun obtenerReclamoVehiculoPorId(
+        id: String
+    ): Resource<ReclamoVehiculo>
+}
