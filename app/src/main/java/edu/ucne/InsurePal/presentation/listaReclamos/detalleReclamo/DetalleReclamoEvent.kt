28d795cc557cdcafae5136a7d1d687ef6d144af1
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-28 22:07:13 -0400

    Crea los eventos para aprobar y rechazar reclamos

--- app/src/main/java/edu/ucne/InsurePal/presentation/listaReclamos/detalleReclamo/DetalleReclamoEvent.kt
+++ app/src/main/java/edu/ucne/InsurePal/presentation/listaReclamos/detalleReclamo/DetalleReclamoEvent.kt
@@ -3,4 +3,7 @@ package edu.ucne.InsurePal.presentation.listaReclamos.detalleReclamo
 sealed interface DetalleReclamoEvent {
     data object OnErrorDismiss : DetalleReclamoEvent
     data object OnReintentar : DetalleReclamoEvent
+
+    data object OnAprobar : DetalleReclamoEvent
+    data class OnRechazar(val motivo: String) : DetalleReclamoEvent
 }
