b551b6d15c3dce4c33e40c99c9461078cf2ef187
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-29 13:36:51 -0400

    Crea los mappers para `ReclamoVida`

---
+++ app/src/main/java/edu/ucne/InsurePal/data/remote/reclamoVida/ReclamoVidaApiService.kt
@@ -0,0 +1,44 @@
+package edu.ucne.InsurePal.data.remote.reclamoVida
+
+import edu.ucne.InsurePal.data.remote.reclamoVida.dto.ReclamoVidaResponse
+import edu.ucne.InsurePal.data.remote.reclamoVida.dto.ReclamoVidaUpdateRequest
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Response
+import retrofit2.http.*
+
+interface ReclamoVidaApiService {
+    @Multipart
+    @POST("api/reclamos-vida")
+    suspend fun crearReclamoVida(
+        // Datos de texto
+        @Part("PolizaId") polizaId: RequestBody,
+        @Part("UsuarioId") usuarioId: RequestBody,
+        @Part("NombreAsegurado") nombreAsegurado: RequestBody,
+        @Part("Descripcion") descripcion: RequestBody,
+        @Part("LugarFallecimiento") lugarFallecimiento: RequestBody,
+        @Part("CausaMuerte") causaMuerte: RequestBody,
+        @Part("FechaFallecimiento") fechaFallecimiento: RequestBody,
+        @Part("NumCuenta") numCuenta: RequestBody,
+
+        @Part actaDefuncion: MultipartBody.Part,
+        @Part identificacion: MultipartBody.Part? = null
+    ): Response<ReclamoVidaResponse>
+
+    @PUT("api/reclamos-vida/{id}/estado")
+    suspend fun actualizarEstadoReclamo(
+        @Path("id") reclamoId: String,
+        @Body request: ReclamoVidaUpdateRequest
+    ): Response<ReclamoVidaResponse>
+
+
+    @GET("api/reclamos-vida")
+    suspend fun obtenerReclamos(
+        @Query("usuario_id") usuarioId: Int? = null
+    ): Response<List<ReclamoVidaResponse>>
+
+    @GET("api/reclamos-vida/{id}")
+    suspend fun obtenerReclamoPorId(
+        @Path("id") reclamoId: String
+    ): Response<ReclamoVidaResponse>
+}
