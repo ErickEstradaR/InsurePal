ecb352b79c5b95ebbb3464b2e1b5729654bbd9a1
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-30 12:49:56 -0400

    Refactor: Agrupa los parámetros de `CrearReclamoVehiculoUseCase`

--- app/src/main/java/edu/ucne/InsurePal/domain/reclamoVehiculo/useCases/CrearReclamoVehiculoUseCase.kt
+++ app/src/main/java/edu/ucne/InsurePal/domain/reclamoVehiculo/useCases/CrearReclamoVehiculoUseCase.kt
@@ -1,6 +1,7 @@
 package edu.ucne.InsurePal.domain.reclamoVehiculo.useCases
 
 import edu.ucne.InsurePal.data.Resource
+import edu.ucne.InsurePal.domain.reclamoVehiculo.model.CrearReclamoVehiculoParams
 import edu.ucne.InsurePal.domain.reclamoVehiculo.model.ReclamoVehiculo
 import edu.ucne.InsurePal.domain.reclamoVehiculo.repository.ReclamoVehiculoRepository
 import jakarta.inject.Inject
@@ -9,19 +10,7 @@ import java.io.File
 class CrearReclamoVehiculoUseCase @Inject constructor(
     private val repository: ReclamoVehiculoRepository
 ) {
-    suspend operator fun invoke(
-        polizaId: String,
-        usuarioId: Int,
-        descripcion: String,
-        direccion: String,
-        tipoIncidente: String,
-        fechaIncidente: String,
-        numCuenta : String,
-        imagen: File
-    ): Resource<ReclamoVehiculo> {
-
-        return repository.crearReclamoVehiculo(
-            polizaId, usuarioId, descripcion, direccion, tipoIncidente, fechaIncidente,numCuenta, imagen
-        )
+    suspend operator fun invoke(params: CrearReclamoVehiculoParams): Resource<ReclamoVehiculo> {
+        return repository.crearReclamoVehiculo(params)
     }
 }
