21cd5f304dbc6bbf948e46d4355353f6ff5b6b86
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-30 12:50:05 -0400

    Refactor: Agrupa los parámetros de `crearReclamoVehiculo`

--- app/src/main/java/edu/ucne/InsurePal/domain/reclamoVehiculo/repository/ReclamoVehiculoRepository.kt
+++ app/src/main/java/edu/ucne/InsurePal/domain/reclamoVehiculo/repository/ReclamoVehiculoRepository.kt
@@ -1,20 +1,12 @@
 package edu.ucne.InsurePal.domain.reclamoVehiculo.repository
 
 import edu.ucne.InsurePal.data.Resource
+import edu.ucne.InsurePal.domain.reclamoVehiculo.model.CrearReclamoVehiculoParams
 import edu.ucne.InsurePal.domain.reclamoVehiculo.model.ReclamoVehiculo
 import java.io.File
 
 interface ReclamoVehiculoRepository {
-    suspend fun crearReclamoVehiculo(
-        polizaId: String,
-        usuarioId: Int,
-        descripcion: String,
-        direccion: String,
-        tipoIncidente: String,
-        fechaIncidente: String,
-        numCuenta : String,
-        imagen: File
-    ): Resource<ReclamoVehiculo>
+    suspend fun crearReclamoVehiculo(params: CrearReclamoVehiculoParams): Resource<ReclamoVehiculo>
 
     suspend fun cambiarEstadoReclamoVehiculo(
         reclamoId: String,
