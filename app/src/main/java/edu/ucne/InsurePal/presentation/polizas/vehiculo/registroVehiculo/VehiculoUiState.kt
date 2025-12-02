baa734ee0a6eafd129e96b2d43d48734dca5a07e
ErickEstradaR <erickestradarosario@gmail.com>
2025-12-02 15:59:30 -0400

    Agrega validación de errores a los campos de `VehiculoUiState`

--- app/src/main/java/edu/ucne/InsurePal/presentation/polizas/vehiculo/registroVehiculo/VehiculoUiState.kt
+++ app/src/main/java/edu/ucne/InsurePal/presentation/polizas/vehiculo/registroVehiculo/VehiculoUiState.kt
@@ -5,20 +5,31 @@ data class VehiculoUiState(
     val isSuccess: Boolean = false,
     val error: String? = null,
 
-
     val usuarioId: Int? = null,
-
     val vehiculoIdCreado: String? = null,
 
-
     val name: String = "",
+    val errorName: String? = null,
+
     val marca: String = "",
+    val errorMarca: String? = null,
     val modelo: String = "",
+    val errorModelo: String? = null,
+
     val anio: String = "",
+    val errorAnio: String? = null,
+
     val color: String = "",
+    val errorColor: String? = null,
+
     val placa: String = "",
+    val errorPlaca: String? = null,
     val chasis: String = "",
+    val errorChasis: String? = null,
+
     val valorMercado: String = "",
+    val errorValorMercado: String? = null,
+
     val coverageType: String = "Full Cobertura",
 
     val marcasDisponibles: List<String> = emptyList(),
